beb5b2ee8b884f6203b9362604130c13ef227f6c
chenlei <chenleib5@126.com>
2019-05-13 20:56:37 +0800

    first commit

---
+++ App/Config/redis.default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by wwwscm.com
+ * Author: chenlei
+ * Date: 2019/5/9
+ * Time: 15:32
+ */
+return [
+    'host' => '47.97.115.77',
+    'port' => 6379,
+    'auth' => 'tFgx40qp',
+    'pool_max_num' => 4,
+    'pool_min_num' => 1,
+    'pool_time_out' => 0.1,
+    'pre' => [
+        'sms_code' => 'sms_code:'
+    ]
+];
